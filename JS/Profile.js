b3f9173a5d8cfdff67f159548be3854efb6b5464
hnhu2670 <2051050327nhu@ou.edu.vn>
2023-08-07 15:55:09 +0700

    7/8

--- JS/Profile.js
+++ JS/Profile.js
@@ -135,7 +135,7 @@ function load_follow() {
     let c = document.getElementById("items");
     c.innerHTML = "";
 
-    fetch(`/JSON/friends.json`)
+    fetch(`JSON/friends.json`)
         .then((res) => {
             return res.json();
         }).then((data) => {
